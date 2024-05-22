5cafcc5c58cf64b24c4388278f6f6bf61f867199
Sazid24Ali <shaikmohammedsazidali@gmail.com>
2024-05-23 01:05:51 +0530

    MainScreen UI Done and created the methods to work
    need to add the functionality...

--- src/main/java/com/lib/library_management/Controller/MainController.java
+++ src/main/java/com/lib/library_management/Controller/MainController.java
@@ -5,13 +5,58 @@ import org.springframework.stereotype.Component;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.ComboBox;
+import javafx.scene.control.Label;
+import javafx.scene.control.TableView;
 import javafx.scene.control.TextField;
+import javafx.scene.input.MouseEvent;
 
 @Component
 public class MainController {
 
     @FXML
+    private Button Admin_AddNewBookIds_Btn;
+
+    @FXML
+    private Button Admin_AddNewBook_Btn;
+
+    @FXML
+    private Button Admin_AvailableBooks_Btn;
+
+    @FXML
+    private Label Admin_Label;
+
+    @FXML
+    private Button Admin_RemoveBooks_Btn;
+
+    @FXML
+    private TableView<?> Std_BooksDisplay_Table;
+
+    @FXML
     private Button Std_Search_Btn;
+
+    @FXML
+    private Button Stu_Add_Btn;
+
+    @FXML
+    private TextField Stu_Name_La_Field;
+
+    @FXML
+    private Label Stu_Name_Label;
+
+    @FXML
+    private TextField Stu_PhNo_La_Field;
+
+    @FXML
+    private Label Stu_PhNo_Label;
+
+    @FXML
+    private Button Stu_Remove_Btn;
+
+    @FXML
+    private TextField Stu_YOP_La_Field;
+
+    @FXML
+    private Label Stu_YOP_Label;
 
     @FXML
     private ComboBox<?> Student_Course_CBox;
@@ -21,5 +66,61 @@ public class MainController {
 
     @FXML
     private TextField Student_Year_Field;
+
+    @FXML
+    private Button issueBook_Btn;
+
+    @FXML
+    private Button returnBook_Btn;
+
+    @FXML
+    void addNewBook(MouseEvent event) {
+
+    }
+
+    @FXML
+    void addNewBookIDs(MouseEvent event) {
+
+    }
+
+    @FXML
+    void addStudent(MouseEvent event) {
+
+    }
+
+    @FXML
+    void availableBooks(MouseEvent event) {
+
+    }
+
+    @FXML
+    void edit_stu_details(MouseEvent event) {
+
+    }
+
+    @FXML
+    void getStudentData(MouseEvent event) {
+
+    }
+
+    @FXML
+    void issueBook(MouseEvent event) {
+
+    }
+
+    @FXML
+    void removeBooks(MouseEvent event) {
+
+    }
+
+    @FXML
+    void removeStudent(MouseEvent event) {
+
+    }
+
+    @FXML
+    void returnBook(MouseEvent event) {
+
+    }
 
 }
