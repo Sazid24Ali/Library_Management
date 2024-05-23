f4a308afb8f17cbacf6bcac198c6e8e716644b8c
Sazid24Ali <shaikmohammedsazidali@gmail.com>
2024-05-24 01:44:44 +0530

    AvailableBooks Ui MainScreen Open AvailableBooks ✔
    Done with the AvailableBooks Ui
    Added the functionality to open the AvailableBooks window when Clicked from the MainScreen

---
+++ src/main/java/com/lib/library_management/Utility/OpenWindow.java
@@ -0,0 +1,50 @@
+package com.lib.library_management.Utility;
+
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
+import org.springframework.stereotype.Component;
+
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Modality;
+import javafx.stage.Stage;
+
+@Component
+public class OpenWindow {
+
+    @Autowired
+    private ApplicationContext context;
+
+    public void openScene(String FileName, String Title, Stage parent) {
+        try {
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("/Scenes/" + FileName + ".fxml"));
+            loader.setControllerFactory(context::getBean);
+            Parent root = loader.load();
+            Stage stage = new Stage();
+
+            // Change the Size here for the main screen
+            double minWidth = root.minWidth(-1); // Passing -1 to get the computed value
+            double minHeight = root.minHeight(-1); // Passing -1 to get the computed value
+
+            // Set the minimum width and height for the stage
+            stage.setMinWidth(minWidth);
+            stage.setMinHeight(minHeight);
+
+            stage.setMaxWidth(minWidth);
+            stage.setMaxHeight(minHeight);
+
+            stage.initModality(Modality.WINDOW_MODAL);
+            stage.initOwner(parent);
+
+            stage.setTitle(Title);
+            stage.setScene(new Scene(root));
+            stage.show();
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+}
