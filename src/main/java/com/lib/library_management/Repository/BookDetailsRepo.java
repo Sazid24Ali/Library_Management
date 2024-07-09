329711888463a2da9877b8de4aeca84d7189bbc5
Sazid24Ali <shaikmohammedsazidali@gmail.com>
2024-07-09 20:19:09 +0530

    removed unwanted imports

--- src/main/java/com/lib/library_management/Repository/BookDetailsRepo.java
+++ src/main/java/com/lib/library_management/Repository/BookDetailsRepo.java
@@ -1,16 +1,12 @@
 package com.lib.library_management.Repository;
 
-import java.util.List;
-
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.lib.library_management.Entity.BookDetailsEntity;
-import com.lib.library_management.Entity.BooksEntity;
 
 @Repository
 public interface BookDetailsRepo extends JpaRepository<BookDetailsEntity, Integer> {
